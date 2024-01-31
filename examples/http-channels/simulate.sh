b4afba6762ae6e07385d7053864964074b705363
Frederic Kettelhoit <kettelhoit@gmail.com>
2024-01-31 15:14:25 +0000

    Add CLI using `clap`

--- examples/http-channels/simulate.sh
+++ examples/http-channels/simulate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 errorhandler () {
-    kill $(jobs -p)
+    kill $(jobs -p) 2>/dev/null
 }
 trap errorhandler ERR EXIT
-cargo run &
+cargo run -- serve &
 sleep 1
-cargo run -- "http://127.0.0.1:8000" "test" 3 &
-cargo run -- "http://127.0.0.1:8000" "test" 2 4 &
-cargo run -- "http://127.0.0.1:8000" "test" 1 3 &
-cargo run -- "http://127.0.0.1:8000" "test" 0 2
+cargo run -- pre "http://127.0.0.1:8000" --session="test" --parties=3 &
+cargo run -- party "http://127.0.0.1:8000" --session="test" --party=2 --input=4 &
+cargo run -- party "http://127.0.0.1:8000" --session="test" --party=1 --input=3 &
+cargo run -- party "http://127.0.0.1:8000" --session="test" --party=0 --input=2
