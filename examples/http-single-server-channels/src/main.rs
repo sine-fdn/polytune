644ed9dc11b3ab1d56e3ba52dc6a928c9820242a
Frederic Kettelhoit <kettelhoit@gmail.com>
2025-03-27 10:15:18 +0000

    Update http single server example

--- examples/http-single-server-channels/src/main.rs
+++ examples/http-single-server-channels/src/main.rs
@@ -2,11 +2,7 @@ use std::{path::PathBuf, process::exit, time::Duration};
 
 use clap::{Parser, Subcommand};
 use http_channel::PollingHttpChannel;
-use polytune::{
-    fpre::fpre,
-    garble_lang::compile,
-    protocol::{mpc, Preprocessor},
-};
+use polytune::{garble_lang::compile, protocol::mpc};
 use tokio::{fs, time::sleep};
 use tracing::debug;
 
@@ -25,19 +21,6 @@ struct Cli {
 enum Commands {
     /// Starts an HTTP server that relays messages between parties.
     Serve,
-    /// Runs a client as a trusted dealer, responsible for correlated randomness.
-    #[command(arg_required_else_help = true)]
-    Pre {
-        /// The endpoint of the server relaying the messages.
-        #[arg(required = true)]
-        url: String,
-        /// A name that uniquely identifies the MPC session on the server.
-        #[arg(short, long)]
-        session: String,
-        /// The number of parties participating in the computation.
-        #[arg(short, long)]
-        parties: usize,
-    },
     /// Runs a client as a party that participates with its own inputs.
     #[command(arg_required_else_help = true)]
     Party {
@@ -59,31 +42,11 @@ enum Commands {
     },
 }
 
-const P_DEALER: usize = 255;
-
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
     match args.command {
         Commands::Serve => server::serve().await,
-        Commands::Pre {
-            url,
-            session,
-            parties,
-        } => {
-            let mut channel = PollingHttpChannel::new(&url, &session, P_DEALER);
-            channel.join().await.unwrap();
-            loop {
-                let joined = channel.participants().await.unwrap();
-                if joined == parties + 1 {
-                    break;
-                } else {
-                    debug!("Waiting for {} parties to join...", parties + 1 - joined);
-                    sleep(Duration::from_secs(1)).await;
-                }
-            }
-            fpre(&mut channel, parties).await.unwrap();
-        }
         Commands::Party {
             url,
             session,
@@ -103,26 +66,17 @@ async fn main() {
             let parties = prg.circuit.input_gates.len();
             loop {
                 let joined = channel.participants().await.unwrap();
-                if joined < parties + 1 {
-                    debug!("Waiting for {} parties to join...", parties + 1 - joined);
+                if joined < parties {
+                    debug!("Waiting for {} parties to join...", parties - joined);
                     sleep(Duration::from_secs(1)).await;
                 } else {
                     break;
                 }
             }
-            let fpre = Preprocessor::TrustedDealer(P_DEALER);
             let p_out: Vec<_> = (0..parties).collect();
-            let output = mpc(
-                &mut channel,
-                &prg.circuit,
-                &input,
-                fpre,
-                p_eval,
-                party,
-                &p_out,
-            )
-            .await
-            .unwrap();
+            let output = mpc(&mut channel, &prg.circuit, &input, p_eval, party, &p_out)
+                .await
+                .unwrap();
             if !output.is_empty() {
                 let result = prg.parse_output(&output).unwrap();
                 println!("\nThe result is {result}");
