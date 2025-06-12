6d57d7eaebb8ae49765ffdb8d9729c49aea31f41
Jonas Schneider-Bensch <jonas@cryspen.com>
2025-06-12 17:10:21 +0200

    Helper script to work around hax issue

---
+++ e.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script extracts to F*, fixes an issue arising from
+# https://github.com/cryspen/hax/issues/1434 and subsequently attempts
+# to typecheck F*
+
+set -euo pipefail
+
+./hax-driver.py extract-fstar
+sed -i 's/((ei_uij_k\.\[ j \] <: Alloc\.Vec\.t_Vec (bool & u128) Alloc\.Alloc\.t_Global)\.\[ ll \])/((ei_uij_k.[ j ] <: Alloc.Vec.t_Vec (bool \& u128) Alloc.Alloc.t_Global).[ ll ] <: (bool \& u128))/' ./proofs/fstar/extraction/Polytune.Faand.fst 
+./hax-driver.py typecheck-fstar
