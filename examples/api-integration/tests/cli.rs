4961795128e94a1f1270014e9d86de8b3b69da8b
robinhundt <24554122+robinhundt@users.noreply.github.com>
2025-08-04 16:19:13 +0200

    Bench API integration for large inputs

--- examples/api-integration/tests/cli.rs
+++ examples/api-integration/tests/cli.rs
@@ -1,5 +1,7 @@
 use std::{
+    env, fs,
     io::{BufRead, BufReader},
+    path::PathBuf,
     process::{Command, Stdio, exit},
     sync::mpsc::channel,
     thread::{self, sleep},
@@ -11,6 +13,7 @@ fn simulate() {
     let millis = 2000;
     println!("\n\n--- {millis}ms ---\n\n");
     let mut children = vec![];
+    let use_big_input = env::var("POLYTUNE_API_INTEGRATION_BIG").is_ok();
     let args = vec!["run", "--release", "--", "--port=8001"];
     let mut cmd = Command::new("cargo")
         .args(args)
@@ -47,9 +50,10 @@ fn simulate() {
     thread::spawn(move || {
         while let Some(Ok(line)) = stdout.next() {
             println!("party0> {line}");
-            if line.contains("Sending [(false, [0]), (false, [0]), (false, [0]), (true, [0]), (true, [1]), (true, [2])] to http://localhost:8002/output") {
-                return s.send(()).unwrap();
-            }
+            if use_big_input && line.contains("(true, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])") ||
+                !use_big_input && line.contains("Sending [(false, [0]), (false, [0]), (false, [0]), (true, [0]), (true, [1]), (true, [2])] to http://localhost:8002/output") {
+                    return s.send(()).unwrap();
+                }
         }
     });
     thread::spawn(move || {
@@ -59,9 +63,21 @@ fn simulate() {
     });
 
     let client = reqwest::blocking::Client::new();
+    let crate_dir: PathBuf = env!("CARGO_MANIFEST_DIR").parse().unwrap();
+    let (pol0, pol1) = if use_big_input {
+        (
+            fs::read_to_string(crate_dir.join("policy0-big.json")).unwrap(),
+            fs::read_to_string(crate_dir.join("policy1-big.json")).unwrap(),
+        )
+    } else {
+        (
+            fs::read_to_string(crate_dir.join("policy0.json")).unwrap(),
+            fs::read_to_string(crate_dir.join("policy1.json")).unwrap(),
+        )
+    };
     client
         .post("http://127.0.0.1:8001/launch")
-        .body(include_str!("../policy1.json"))
+        .body(pol1)
         .header("Content-Type", "application/json")
         .send()
         .unwrap();
@@ -70,8 +86,9 @@ fn simulate() {
     let client = reqwest::blocking::Client::new();
     client
         .post("http://127.0.0.1:8000/launch")
-        .body(include_str!("../policy0.json"))
+        .body(pol0)
         .header("Content-Type", "application/json")
+        .timeout(Duration::from_secs(60 * 60))
         .send()
         .unwrap();
 
