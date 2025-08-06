7ac8e9d68ba65b857a798e82ef58943010fc4338
robinhundt <24554122+robinhundt@users.noreply.github.com>
2025-08-06 12:16:14 +0200

    Add bench_large_join workflow

--- examples/api-integration/tests/cli.rs
+++ examples/api-integration/tests/cli.rs
@@ -8,15 +8,30 @@ use std::{
     time::Duration,
 };
 
+use serde::Deserialize;
+
 #[test]
 fn simulate() {
     let millis = 2000;
     println!("\n\n--- {millis}ms ---\n\n");
     let mut children = vec![];
     let use_big_input = env::var("POLYTUNE_API_INTEGRATION_BIG").is_ok();
-    let args = vec!["run", "--release", "--", "--port=8001"];
-    let mut cmd = Command::new("cargo")
-        .args(args)
+    let heaptrack_profiling = env::var("POLYTUNE_API_INTEGRATION_HEAPTRACK").is_ok();
+    let polytune_bin = PathBuf::from(env!("CARGO_TARGET_TMPDIR"))
+        .join("../debug-release/polytune-api-integration");
+    let cmd_prog = if heaptrack_profiling {
+        "heaptrack".into()
+    } else {
+        polytune_bin.clone()
+    };
+    let common_args = if heaptrack_profiling {
+        vec!["--record-only", polytune_bin.to_str().unwrap()]
+    } else {
+        vec![]
+    };
+    let mut cmd = Command::new(&cmd_prog)
+        .args(common_args.clone())
+        .arg("--port=8001")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
@@ -36,9 +51,10 @@ fn simulate() {
     children.push(cmd);
 
     sleep(Duration::from_millis(millis));
-    let args = vec!["run", "--release", "--", "--port=8000"];
-    let mut cmd = Command::new("cargo")
-        .args(args)
+
+    let mut cmd = Command::new(cmd_prog)
+        .args(common_args)
+        .arg("--port=8000")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
@@ -50,10 +66,16 @@ fn simulate() {
     thread::spawn(move || {
         while let Some(Ok(line)) = stdout.next() {
             println!("party0> {line}");
-            if use_big_input && line.contains("(true, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])") ||
-                !use_big_input && line.contains("Sending [(false, [0]), (false, [0]), (false, [0]), (true, [0]), (true, [1]), (true, [2])] to http://localhost:8002/output") {
+            if use_big_input {
+                let false_match = "(false, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])";
+                let expected_literal = vec![false_match; 1000 + 1000 - 1].join(", ");
+                let expected_msg = format!("[{expected_literal}]");
+                if line.contains(&expected_msg) {
                     return s.send(()).unwrap();
                 }
+            } else if line.contains("Sending [(false, [0]), (false, [0]), (false, [0]), (true, [0]), (true, [1]), (true, [2])] to http://localhost:8002/output") {
+                return s.send(()).unwrap();
+            }
         }
     });
     thread::spawn(move || {
@@ -75,6 +97,17 @@ fn simulate() {
             fs::read_to_string(crate_dir.join("policy1.json")).unwrap(),
         )
     };
+    #[derive(Deserialize)]
+    struct PolicyProgram {
+        program: String,
+    }
+    let pol0_program: PolicyProgram = serde_json::from_str(&pol0).expect("Invalid policy");
+    let pol1_program: PolicyProgram = serde_json::from_str(&pol1).expect("Invalid policy");
+    let expected_policy = fs::read_to_string(crate_dir.join(".example.garble.rs"))
+        .expect("error reading example garble program");
+    assert_eq!(expected_policy, pol0_program.program);
+    assert_eq!(expected_policy, pol1_program.program);
+
     client
         .post("http://127.0.0.1:8001/launch")
         .body(pol1)
